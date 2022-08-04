9cb09fc6139435a9eb391f9c6731ad5463de0879
katla20 <katla7775@gmail.com>
2022-08-04 10:49:05 -0500

    1 file modified

--- index.js
+++ index.js
@@ -116,3 +116,27 @@ let start1 = [0, 2];
 let end1 = [1, 1];
 console.log('case 2: ');
 myObj.shortestPath(matrix, start1, end1);
+
+// permutes(permutation: Array<Function>) {
+//   debugger;
+//   let length = permutation.length,
+//     result = [permutation.slice()],
+//     c = new Array(length).fill(0),
+//     i = 1, k, p;
+
+//   while (i < length) {
+//     if (c[i] < i) {
+//       k = i % 2 && c[i];
+//       p = permutation[i];
+//       permutation[i] = permutation[k];
+//       permutation[k] = p;
+//       ++c[i];
+//       i = 1;
+//       result.push(permutation.slice());
+//     } else {
+//       c[i] = 0;
+//       ++i;
+//     }
+//   }
+//   return result;
+// }
